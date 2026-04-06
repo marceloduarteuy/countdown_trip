c40467aea92009a6131ad33876ee8de6105cce28
Marcelo Duarte <marcelo.duarte@360dialog.com>
2026-04-06 11:24:13 -0300

    eee
    eee

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
 
 self.addEventListener("activate", e => {
   console.log("Service Worker activo");
 });
 
-self.addEventListener("fetch", e => {
-  console.log("Service Worker fetch");
+// Este código permite que la PWA funcione offline y sea instalable
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('bebo-store').then((cache) => cache.addAll([
+      './index.html',
+      './manifest.json',
+      './foto_512x512.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
